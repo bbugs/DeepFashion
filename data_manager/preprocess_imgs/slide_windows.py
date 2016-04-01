9f66b598f80d506373e36eb8f75ead29f822d005
bbugs <ndo.hoffman@gmail.com>
2016-04-01 21:11:16 +0200

    cleaning up

--- data_manager/preprocess_imgs/slide_windows.py
+++ data_manager/preprocess_imgs/slide_windows.py
@@ -3,7 +3,7 @@ import os
 from PIL import Image
 
 n_regions = 4
-n_img_per_folder = 1000 / n_regions  # 250 for 4 regions per image.  1000 is number of files per folder
+n_region_per_folder = 1000 / n_regions  # 250 for 4 regions per image.  1000 is number of files per folder
 
 # out_folder = '<n_regions>_regions/<folder_id>'  e.g. 4_regions/0
 # folder_id = img_id / (n_img_per_folder)
@@ -21,7 +21,7 @@ out_path = '../../data/fashion53k/img_regions/{}_regions_imgs/'.format(n_regions
 for item in data['items']:
     img_id = item['imgid']
     split = item['split']
-    asin = item['asin']`
+    asin = item['asin']
     folder = item['folder']
     img_filename = item['img_filename']
     # print folder, img_filename
@@ -35,7 +35,7 @@ for item in data['items']:
     y0 = 0
     y1 = h / n_regions
     # Specify folder to save the img regions
-    folder_id = img_id / n_img_per_folder  # img_id / 250 when n_regions=4
+    folder_id = img_id / n_region_per_folder  # img_id / 250 when n_regions=4
     region_path = out_path + str(folder_id) + '/'
     if not os.path.exists(region_path):
         os.makedirs(region_path)
